2aeed7f1912156faf63dd64467a3930f3143fb25
Jeferson Almeida <jeferson.live11@gmail.com>
2024-07-18 11:54:38 -0300

    exemplos

---
+++ exemplo.py
@@ -0,0 +1,23 @@
+# Exercício
+## Crie um programa que o usuário digita seu nome e retorna a quantidade de caracteres
+
+#len utiliza-se para contar a quantidade de caracteres
+    #print(len(input("Digite o seu nome: ")))
+
+# Exercício
+## Criar um programa onde o usuário digite dois valores e apareça a soma
+
+    #print(int(input("Digite o primeiro número: ")) + int(input("Digite o segundo número: ")))
+
+nome =input("Digite o promeiro número: ")
+quantidade_de_caracteres = len(nome)
+print(quantidade_de_caracteres)
+
+# Tipos de dados
+## Tipos primitivoas de python
+
+    #numero = 3 #int
+    #numero_decimal = 4.12 #float
+    #nome_de_usuario = "Jeferson" #string
+    #verdadeiro = True #bool
+
