47eea68b5b7b12e531388abe59ec7e048919c512
Jeferson Almeida <jeferson.live11@gmail.com>
2024-07-18 11:54:09 -0300

    salvando meu main

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# Códigos
+
 print("Hello world!")
+
+print("Testando alguns" + " " + "comandos do python")
+
+print("em 2024")
+
+print(4 + 4)
+
+print(input("Digite seu nome:"))
+
+print("Olá, " + input("Digite seu nome: ") + "!")
+
+
+
