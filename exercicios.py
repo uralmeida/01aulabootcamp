a3596c994b2c9276d1d4b048f57ef34f24248dd5
Jeferson Almeida <jeferson.live11@gmail.com>
2024-07-23 14:13:59 -0300

    meus exercicios

---
+++ exercicios.py
@@ -0,0 +1,11 @@
+# Exercício 01
+## Crie um programa que o usuário digita seu nome e retorna a quantidade de caracteres
+
+# len utiliza-se para contar a quantidade de caracteres
+
+print(len(input("Digite o seu nome: ")))
+
+# Exercício 02
+## Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite o primeiro número: ")) + int(input("Digite o segundo número: ")))
